343d0f517c6ea09e9a2556e872c9ddf8c558f34c
GregoryArmstrong <intergalacticgentleman@gmail.com>
2016-03-08 14:55:01 -0700

    Add graphs to master_league#comparison
    All graphs added to comparison show screen. Styling changes inbound.

--- app/controllers/master_league_controller.rb
+++ app/controllers/master_league_controller.rb
@@ -14,29 +14,72 @@ class MasterLeagueController < ApplicationController
     @user = User.find(params[:user_id])
     @presenter = Presenter.new(@user)
     @user_stats = @presenter.recent_games_averages(@user).averages
-    @pro_stats = Rails.cache.read("10_master_player_games_averages").select { |player| player.summoner_name == params[:comparison][:summoner_name]}.first
+    @pro_stats = Rails.cache.read("master-league-player-games-averages").select { |player| player.summoner_name == params[:comparison][:summoner_name]}.first
     @kda_chart = create_kda_graph_chart(@user, @user_stats, @pro_stats)
-    @other_chart = create_other_graph_chart(@user, @user_stats, @pro_stats)
+    @ward_chart = create_ward_graph_chart(@user, @user_stats, @pro_stats)
+    @creep_chart = create_creep_score_graph_chart(@user, @user_stats, @pro_stats)
+    @user_pie_chart = champions_pie_chart(@user_stats)
+    @pro_pie_chart = champions_pie_chart(@pro_stats.averages)
   end
 
   private
 
-  def create_other_graph_chart(user, user_stats, pro_stats)
+  def champions_arrays(champions_info)
+    total_games = champions_info.values.reduce(:+)
+    champions_info.map do |champion|
+      [champion[0][:name], (champion[1].to_f / total_games.to_f)*100]
+    end
+  end
+
+  def champions_pie_chart(stats)
+    LazyHighCharts::HighChart.new('pie') do |f|
+      f.chart({:defaultSeriesType => "pie"})
+      series = { :type => 'pie',
+                 :name => "Percent Played",
+                 :data => champions_arrays(stats[:champions_ids])
+                }
+      f.series(series)
+      f.legend(:layout => 'vertical',
+               :style => {:left => 'auto',
+                          :bottom => 'auto',
+                          :right => '50px',
+                          :top => '100px'})
+      f.plot_options(:pie => {:allowPointSelect => true,
+                              :cursor => 'pointer',
+                              :dataLabels => {:enabled => true,
+                                              :color => 'black',
+                                              :style => {:fontSize => "25px"}
+                                             }
+                             }
+                    )
+    end
+  end
+
+  def create_creep_score_graph_chart(user, user_stats, pro_stats)
     LazyHighCharts::HighChart.new('graph') do |f|
-      f.options[:xAxis][:categories] = ['Creep Score',
-                                        'Wards Placed',
+      f.options[:xAxis][:categories] = ['Creep Score']
+      f.series(:type => 'column',
+               :name => "#{user.summoner_name}",
+               :data => [user_stats[:creep_scores]])
+      f.series(:type => 'column',
+               :name => "#{pro_stats.summoner_name}",
+               :data => [user_stats[:creep_scores]])
+    end
+  end
+
+  def create_ward_graph_chart(user, user_stats, pro_stats)
+    LazyHighCharts::HighChart.new('graph') do |f|
+      f.options[:xAxis][:categories] = ['Wards Placed',
                                         'Vision Wards Placed',
                                         'Wards Destroyed']
       f.series(:type => 'column',
                :name => "#{user.summoner_name}",
-               :data => [user_stats[:creep_scores],
-                         user_stats[:wards_placed],
+               :data => [user_stats[:wards_placed],
                          user_stats[:vision_wards],
                          user_stats[:wards_destroyed]])
       f.series(:type => 'column',
                :name => "#{pro_stats.summoner_name}",
-               :data => [pro_stats.averages[:creep_scores],
-                         pro_stats.averages[:wards_placed],
+               :data => [pro_stats.averages[:wards_placed],
                          pro_stats.averages[:vision_wards],
                          pro_stats.averages[:wards_destroyed]])
      end
--- app/models/master_league_player_builder.rb
+++ app/models/master_league_player_builder.rb
@@ -44,8 +44,7 @@ class MasterLeaguePlayerBuilder
                       expires_in: 1.hours) do
       master_league_players_info.map do |player|
         sleep(1.0)
-        player.averages = GameDataAverage.new(recent_games(player))
-        player.save
+        player.averages = recent_games_averages(player).averages
         player
       end
     end
