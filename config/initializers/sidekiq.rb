503653674a6f7165b2e5bfa3970ce5e44f8ad043
GregoryArmstrong <intergalacticgentleman@gmail.com>
2016-03-09 11:05:13 -0700

    change client and server for sidekiq to use redistogo_url env variable

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -7,5 +7,6 @@ Sidekiq.configure_server do |config|
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379/12' }
+  config.redis = { url: ENV['REDISTOGO_URL']}
+  # config.redis = { url: 'redis://localhost:6379/12' }
 end
